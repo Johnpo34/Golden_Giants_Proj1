b7269b47968a1072c6299036664efaf60964d4de
Tyler Gandy <tylerbgandy@gmail.com>
2022-01-18 20:56:42 -0500

    Changes made to button layout and javascript to interact with buttons

--- Java/js.js
+++ Java/js.js
@@ -1,22 +1,35 @@
 var translateButton=document.getElementById("download-button")
 var inputData = document.querySelector('.userInput')
+var showForm = document.getElementById('showForm')
+var selectButton = document.getElementById('selectButton')
+var arabicSelect = document.getElementById('arabicCheck')
+var spanishSelect = document.getElementById('spanishCheck')
+var frenchSelect = document.getElementById('frenchCheck')
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var drop = document.querySelectorAll('.dropdown-trigger');
-    M.Dropdown.init(drop);
+    // var drop = document.querySelectorAll('.dropdown-trigger');
+    // M.Dropdown.init(drop);
+    
+
+
   });
 
 
 
  async function runFetch(input){
 
+// if (arabicSelect.checked){
+//   targetLanguage=""
+// }
 
 
   var res = await fetch("https://libretranslate.de/translate", {
     method: "POST",
     body: JSON.stringify({
       q: input.value,
+
+
       source: "en",
       target: "es",
       format: "text"
@@ -33,15 +46,19 @@ document.addEventListener('DOMContentLoaded', function() {
   event.preventDefault();
 console.log(event.target);
 runFetch(inputData);
-var dataObject = {
-}
 
 var userInput = inputData.value;
 console.log(userInput);
-localStorage.setItem("userInput",dataObject);
+localStorage.setItem("lastEntry", userInput);
 // inputData=localStorage.setItem('input-text');
 
 // the IF STATEMENTS GO HERE 
 
 })
 
+selectButton.addEventListener("click", function(event){
+  event.preventDefault()
+  console.log("this works")
+  showForm.setAttribute("style", "display:block")
+  
+})
