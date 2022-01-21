3730c2a44175a00a26d210da79d5cb94ea4fc79f
Tyler Gandy <tylerbgandy@gmail.com>
2022-01-20 21:32:32 -0500

    edit to javascript to get translated text to show on page and stored in local storage

--- Java/js.js
+++ Java/js.js
@@ -1,27 +1,35 @@
-var translateButton=document.getElementById("download-button")
+var translateButton = document.getElementById("download-button")
 var inputData = document.querySelector('.userInput')
 var showForm = document.getElementById('showForm')
 var selectButton = document.getElementById('selectButton')
 var arabicSelect = document.getElementById('arabicCheck')
 var spanishSelect = document.getElementById('spanishCheck')
 var frenchSelect = document.getElementById('frenchCheck')
+var sectionForTranslate = document.getElementById('addTranslation')
+var userInput = inputData.value
+
+document.addEventListener('DOMContentLoaded', function () {
+  // var drop = document.querySelectorAll('.dropdown-trigger');
+  // M.Dropdown.init(drop);
+  init();
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    // var drop = document.querySelectorAll('.dropdown-trigger');
-    // M.Dropdown.init(drop);
-    
-
-
-  });
+});
 
 
 
- async function runFetch(input){
+async function runFetch(input) {
 
-// if (arabicSelect.checked){
-//   targetLanguage=""
-// }
+  console.log(arabicCheck.checked)
+  if (arabicCheck.checked) {
+    targetLanguage = "ar"
+  }
+  else if (spanishCheck.checked) { targetLanguage = "es" }
+
+  else { targetLanguage = "fr" }
+  // if (arabicSelect.checked){
+  //   targetLanguage=""
+  // }
 
 
   var res = await fetch("https://libretranslate.de/translate", {
@@ -31,34 +39,86 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
       source: "en",
-      target: "es",
+      target: targetLanguage,
       format: "text"
     }),
     headers: { "Content-Type": "application/json" }
   });
-  
 
 
-  console.log(await res.json());
- }
 
- translateButton.addEventListener("click", function(event){
+  var inputTranslated = await res.json()
+
+  return inputTranslated
+
+
+}
+
+translateButton.addEventListener("click", function (event) {
   event.preventDefault();
-console.log(event.target);
-runFetch(inputData);
+  console.log(event.target);
+  runFetch(inputData).then(function (translatedText) {
+    console.log(translatedText)
 
-var userInput = inputData.value;
-console.log(userInput);
-localStorage.setItem("lastEntry", userInput);
-// inputData=localStorage.setItem('input-text');
 
-// the IF STATEMENTS GO HERE 
 
+    localStorage.setItem("userEntries", JSON.stringify(userEntries))
+
+    var userInput = inputData.value
+    var lastSearch = localStorage.setItem("previousSearch", userInput)
+    var firstSearch = localStorage.getItem("previousSearch")
+    // userEntries.push(firstSearch)
+    if (inputData.value !== lastSearch) {
+    }
+    console.log(userEntries)
+    localStorage.setItem("firstSearch", firstSearch)
+
+
+    var userInput = inputData.value;
+    console.log(userInput);
+    // localStorage.setItem("lastEntry", storedE);
+    // inputData=localStorage.setItem('input-text');
+
+    // the IF STATEMENTS GO HERE 
+
+    showOnPage(translatedText)
+  });
 })
 
-selectButton.addEventListener("click", function(event){
+
+var userEntries = []
+
+function init() {
+  var storedEntries = JSON.parse(localStorage.getItem("userEntries"))
+  if (storedEntries !== null) {
+    userEntries = storedEntries;
+  }
+}
+
+selectButton.addEventListener("click", function (event) {
   event.preventDefault()
   console.log("this works")
   showForm.setAttribute("style", "display:block")
-  
+
 })
+
+
+//Show translation on Page
+
+function showOnPage(userInput) {
+  console.log(userInput)
+  var newDiv = document.createElement("div")
+  var showOnPage = document.createElement('h3')
+  showOnPage.setAttribute("class", "addText")
+  showOnPage.setAttribute("type", "text");
+  showOnPage.innerText = userInput.translatedText
+  showOnPage.style.textcolor = "black"
+  newDiv.append(showOnPage)
+  sectionForTranslate.append(newDiv)
+
+
+}
+// var showOnPage = document.createElement('h1')
+// var innertext = response.translatedtext
+// showOnPage.innerText = response
+// sectionForTranslate.append(showOnPage)
